09eac6fd14d73a9465b5ebbcb060229d42b3de34
Joseph Onsase <99542024+ripper19@users.noreply.github.com>
2024-06-05 23:59:07 +0300

    nAmes

--- api/apps.php
+++ api/apps.php
@@ -8,7 +8,7 @@
 	<meta name="viewport"
 		content="width=device-width, 
 				initial-scale=1.0">
-	<link rel="stylesheet" href="apps.css">
+	<link rel="stylesheet" href="app.css">
 	<title>Dynamic Calendar</title>
 </head>
 
